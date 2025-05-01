d5902c398b4e856c9c6cb84c6255210af20b08d3
Raphaël Ribes <raphael.ribes@gmail.com>
2025-05-01 17:59:27 +0200

    temp

--- plots.py
+++ plots.py
@@ -8,7 +8,6 @@ from scipy import stats
 def plothastaglinkscounts(dataset):
     mentioncount = []
     linkscount = []
-    hashtagcount = []
 
     categories = ["scientific_claim", "scientific_reference", "scientific_context"]
 
@@ -16,20 +15,16 @@ def plothastaglinkscounts(dataset):
         subset = dataset[dataset[cat] == 1]
         mentioncount.append(subset["text"].str.count("@").sum())
         linkscount.append(subset["text"].str.count("http").sum())
-        hashtagcount.append(subset["text"].str.count("#").sum())
 
     # --- Plotting the grouped bar chart ---
     x = np.arange(len(categories))  # the label locations
-    width = 0.2  # the width of the bars
+    width = 0.4  # the width of the bars
 
     fig, ax = plt.subplots(figsize=(12, 7))
     rects1 = ax.bar(
-        x - width, mentioncount, width, label="Mentions", color="green"
+        x - width/2, mentioncount, width, label="Mentions", color="green"
     )
-    rects2 = ax.bar(x, linkscount, width, label="Links", color="orange")
-    rects3 = ax.bar(
-        x + width, hashtagcount, width, label="Hashtags", color="blue"
-    )  # Changed color
+    rects2 = ax.bar(x + width/2, linkscount, width, label="Links", color="orange")
 
     # Add some text for labels, title and axes ticks
     ax.set_ylabel("Count")
@@ -67,19 +62,9 @@ def plothastaglinkscounts(dataset):
                     print(f"Category '{categories[i]}' (Links): Count={height}, p-value={p_value:.3f} (Outlier)")
                 else:
                     print(f"Category '{categories[i]}' (Links): Count={height}, p-value={p_value:.3f}")
-            elif rects is rects3:
-                is_outlier, p_value = check_outlier(hashtagcount, height)
-                if is_outlier:
-                    ax.annotate("Outlier", xy=(rect.get_x() + rect.get_width() / 2, height),
-                                xytext=(0, -15), textcoords="offset points",
-                                ha="center", va="top", color='red', fontsize=8)
-                    print(f"Category '{categories[i]}' (Hashtags): Count={height}, p-value={p_value:.3f} (Outlier)")
-                else:
-                    print(f"Category '{categories[i]}' (Hashtags): Count={height}, p-value={p_value:.3f}")
 
     autolabel(rects1)
     autolabel(rects2)
-    autolabel(rects3)
 
     fig.tight_layout()
     plt.savefig("rapport/images/hashtag_links_mentions_count_outliers.png", dpi=300)
@@ -147,8 +132,43 @@ def model_comparison_sci_nsci(path):
     plt.savefig(f"rapport/images/{path.split('.')[0]}.png", dpi=300)
     plt.show()
 
+def plot_confusion_matrix(path, model_name):
+    with open(path , "r") as f:
+        data = json.load(f)
+
+    key_index = data["keys"].index(model_name)
+    values: dict = data["values"][key_index]
+
+    for label, matrix in values.items():
+        plt.figure(figsize=(8, 6))
+        plt.imshow(matrix, interpolation='nearest', cmap=plt.cm.Blues)
+        plt.title(f"Confusion Matrix for {model_name} - {label}")
+        plt.colorbar()
+        plt.xticks([0, 1], ["True", "False"])
+        plt.yticks([0, 1], ["True", "False"])
+        matrix = np.array(matrix)
+        # add values
+        threshold = matrix.max() / 2.
+        for i in range(matrix.shape[0]):
+            for j in range(matrix.shape[1]):
+                plt.text(j, i, format(matrix[i, j], 'd'),
+                         horizontalalignment="center",
+                         color="white" if matrix[i, j] > threshold else "black")
+
+        plt.ylabel('True label')
+        plt.xlabel('Predicted label')
+        plt.tight_layout()
+        plt.savefig(f"rapport/images/{path}-{model_name}_{label}_confusion_matrix.png", dpi=300)
+        plt.show()
+
+
 if __name__ == "__main__":
-    model_comparison_sci_nsci("model_comparison.json")
+    model_comparison_sci_nsci("model_comparison_1.json")
+    model_comparison_sci_nsci("model_comparison_3.json")
+    model_comparison_sci_nsci("model_comparison_2.json")
+
+    plot_confusion_matrix("confusion_2.json", "Random Forest")
+    plot_confusion_matrix("confusion_3.json", "Random Forest")
     # Load the dataset
     try:
         dataset = pd.read_csv('scitweets_export.tsv', sep='\t')
